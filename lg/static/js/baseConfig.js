2e757ce043a530601d9a93330bb4b1a05589c050
dzbpyz <dzbpyz@gmail.com>
2025-05-12 17:19:57 +0800

    11

---
+++ lg/static/js/baseConfig.js
@@ -0,0 +1,7 @@
+﻿var baseConfig={
+    "printProxyName":"print-proxy-fg01.html",
+    "baseUrl":"http://qac.hbislt.com:10091/",
+    "frUrl":"http://qac.hbislt.com:10093/",
+    "opAjaxUrl":"http://qac.hbislt.com:10092/"
+}
+sessionStorage.setItem("baseConfig",JSON.stringify(baseConfig));
